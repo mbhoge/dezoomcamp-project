616ca218d45e929b2e03cf3530e40d39dbf72b00
Manish <bhoge.m@outlook.com>
2023-05-01 09:07:26 +0000

    data files

--- marketdatapush/src_to_gcloud.py
+++ marketdatapush/src_to_gcloud.py
@@ -7,57 +7,44 @@ import pandas as pd
 from sqlalchemy import create_engine
 from prefect import flow, task
 from prefect.tasks import task_input_hash
-from datetime import timedelta
-from prefect_sqlalchemy import SqlAlchemyConnector
 
 
-@task(log_prints=True, tags=["extract"], cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
-def extract_data(url: str):
-    # the backup files are gzipped, and it's important to keep the correct extension
-    # for pandas to be able to open the file
-    if url.endswith('.csv.gz'):
-        csv_name = 'yellow_tripdata_2021-01.csv.gz'
-    else:
-        csv_name = 'output.csv'
+# @task(log_prints=True)
+
+def read_txt_files_from_folder(folder_path):
+    file_list = [f for f in os.listdir(folder_path) if f.endswith('.txt')]
+    df_list = []
+    for file_name in file_list:
+        file_path = os.path.join(folder_path, file_name)
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            data = [line.strip().split(',') for line in lines]
+            df_list.append(pd.DataFrame(data))
+    combined_df = pd.concat(df_list, ignore_index=True)
+    return combined_df
+
+""" def upload_to_gcs(bucket, object_name, local_file):
     
-    os.system(f"wget {url} -O {csv_name}")
+    # # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
+    # # (Ref: https://github.com/googleapis/python-storage/issues/74)
+    # storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
+    # storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
 
-    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+    blob = bucket.blob(object_name)
+    blob.upload_from_filename(local_file)
+"""
 
-    df = next(df_iter)
-
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-
-    return df
-
-@task(log_prints=True)
-def transform_data(df):
-    print(f"pre: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
-    df = df[df['passenger_count'] != 0]
-    print(f"post: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
-    return df
-
-@task(log_prints=True, retries=3)
-def load_data(table_name, df):
-    
-    connection_block = SqlAlchemyConnector.load("postgres-connector")
-    with connection_block.get_connection(begin=False) as engine:
-        df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
-        df.to_sql(name=table_name, con=engine, if_exists='append')
-
-@flow(name="Subflow", log_prints=True)
+# @flow(name="Subflow", log_prints=True)
 def log_subflow(table_name: str):
     print(f"Logging Subflow for: {table_name}")
 
-@flow(name="Ingest Data")
-def main_flow(table_name: str = "yellow_taxi_trips"):
-
-    csv_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
-    log_subflow(table_name)
-    raw_data = extract_data(csv_url)
-    data = transform_data(raw_data)
-    load_data(table_name, data)
+# @flow(name="Ingest Data")
+def main_flow(datasource: str):
+    folder_path =  'C:\\Users\\Manish_Bhoge\\OneDrive - EPAM\\Tut\\DataEngineering-zoomcamp\\Project\\Huge Stock Market Dataset\\Data\\Stocks'
+    df = read_txt_files_from_folder(folder_path)
+    print(df.head())
 
 if __name__ == '__main__':
-    main_flow(table_name = "yellow_trips")
+    main_flow(datasource = "stocks")
